9a009d5fc91a3f7a4cecfafc2d5da99ce0c64ca3
gjjoo <joogeunjae@gmail.com>
2023-01-03 13:40:21 +0900

    feat: 테마 필터 추가

--- app/routers/themes.py
+++ app/routers/themes.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from fastapi import APIRouter
+from prisma import types
 
 from app.prisma import prisma
 from app.utils.find_many_cursor import find_many_cursor
@@ -15,14 +16,19 @@ router = APIRouter(
 @router.get("")
 async def get_themes(
     term: Optional[str] = None,
-    genre: Optional[str] = None,
     areaB: Optional[str] = None,
-    level: Optional[float] = None,
-    lockingRatio: Optional[int] = None,
+    level: Optional[int] = None,
+    person: Optional[int] = None,
+    minPrice: Optional[int] = None,
+    maxPrice: Optional[int] = None,
+    fearScore: Optional[str] = None,
+    activity: Optional[str] = None,
+    minLockingRatio: Optional[int] = None,
+    maxLockingRatio: Optional[int] = None,
     take: Optional[int] = 20,
     cursor: Optional[str] = None,
 ):
-    options = {
+    options: types.FindManyThemeArgsFromTheme = {
         "take": take + 1,
         "where": {"status": "PUBLISHED"},
         "include": {
@@ -33,14 +39,46 @@ async def get_themes(
     }
     if term:
         options["where"]["name"] = {"contains": term}
-    if genre:
-        options["where"]["genre"] = {"some": {"id": genre}}
     if areaB:
         options["where"]["cafe"] = {"areaB": areaB}
     if level:
         options["where"]["level"] = level
-    if lockingRatio:
-        options["where"]["lockingRatio"] = lockingRatio
+    if person:
+        options["where"]["minPerson"] = {"lte": person}
+        options["where"]["maxPerson"] = {"gte": person}
+    if minPrice or maxPrice:
+        if minPrice and maxPrice:
+            options["where"]["price"] = {"gte": minPrice, "lte": maxPrice}
+        elif minPrice:
+            options["where"]["price"] = {"gte": minPrice}
+        elif maxPrice:
+            options["where"]["price"] = {"lte": maxPrice}
+    if fearScore:
+        if fearScore == "hight":
+            options["where"]["fear"] = {"gte": 4}
+        elif fearScore == "low":
+            options["where"]["fear"] = {"gte": 1, "lte": 2}
+        else:
+            options["where"]["fear"] = {"gt": 2, "lt": 4}
+    if activity:
+        if activity == "hight":
+            options["where"]["activity"] = {"gte": 4}
+        elif activity == "low":
+            options["where"]["activity"] = {"gte": 1, "lte": 2}
+        else:
+            options["where"]["activity"] = {"gt": 2, "lt": 4}
+    if minLockingRatio or maxLockingRatio:
+        if minLockingRatio and maxLockingRatio:
+            options["where"]["lockingRatio"] = {
+                "gte": minLockingRatio,
+                "lte": maxLockingRatio,
+            }
+        elif minLockingRatio:
+            options["where"]["lockingRatio"] = {"gte": minLockingRatio}
+        elif maxLockingRatio:
+            options["where"]["lockingRatio"] = {"lte": maxLockingRatio}
+    # if genre:
+    #     options["where"]["genre"] = {"some": {"id": genre}}
     if cursor:
         options["cursor"] = {"id": cursor}
 
